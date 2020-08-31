fadd651686f2a8864f9ef19c15cb2694bd8815da
hseunghye93 <hseunghye93@gmail.com>
2020-08-31 15:55:12 +0900

    vue-jquery

---
+++ vue-jquery/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    App 컴포넌트 <br />
+    <DatePicker></DatePicker>
+  </div>
+</template>
+
+<script>
+// 뷰 로직에 제이쿼리 로직 섞어서 개발하다 보면
+// 추후 코드를 추적, 관리 어려움.
+// 날짜 선택기 위젯 부분만 컴포넌트로 분리하여 관리할 수 있게 코드를 구조화.
+import DatePicker from './DatePicker.vue'
+export default {
+  name: 'app'
+  , data(){
+    return{
+      msg: 'Welcome to Yourt Vue.js App'
+    }
+  }
+  , components:{
+    // 'DatePicker': DatePicker
+    // 와 동일.. ES6의 객체 리터럴에서 제공하는 약식 문법
+    DatePicker
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ vue-jquery/src/DatePicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <input id="calendar">
+</template>
+
+<script>
+export default {
+  // created(){
+  //   console.log($.fn.jquery);
+  //   console.log($.ui.version);
+  // }
+  // 중요 !!
+  // 인스턴스 라이프 사이클 훅 중 mounted 에 제이쿼리 로직을 추가
+  // 버전 확인은 created 에 로직 수행할 때는 mounted 를 사용한 이유
+  // <template> 안의 HTML 태그에 접근할 수 있는 시점이
+  // 뷰의 가상 돔이 화면의 특정 요소에 부착되고 난 시점인 mounted 이기 때문
+  // mounted 이전 라이프 사이클 단계인 created 에서는 <template> 에 저의한
+  // 돔 요소에 접근 할 수 x
+  mounted(){
+    $('#calendar').datepicker();
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
