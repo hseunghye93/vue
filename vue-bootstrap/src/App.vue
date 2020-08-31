7b0d7f21d88bbfb40722f6b93b2c792a61d702a8
hseunghye93 <hseunghye93@gmail.com>
2020-08-31 17:31:37 +0900

    vue-bootstrap

---
+++ vue-bootstrap/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import Chart from './Chart.vue'
+
+export default {
+  name: 'app'
+  , data(){
+    return{
+      msg: 'Welcome to Yourt Vue.js App'
+    }
+  }
+  , components:{
+    Chart
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ vue-bootstrap/src/Chart.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script>
+export default {
+  mounted(){
+      Highcharts.chart('container',{
+        chart:{
+          type:'bar'
+        }
+        ,title:{
+          text: 'Fruit Consumption'
+        }
+        , xAXIS:{
+          categories: ['Apples','Bananas','Oranges']
+        }
+        , yAXIS:{
+          title: {
+            text:'Fruit eaten'
+          }
+        }
+        ,series: [{
+          name:'Jane'
+          ,data:[1,0,4]
+        },{
+          name:'John'
+          ,data:[5,7,3]
+        }]
+      });
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  #container{
+    width: 100%;
+    height: 400px;
+  }
+</style>
